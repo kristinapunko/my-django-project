f6c7323872c35fa43e9bd48853566d14ae07761c
kristinapunko <146848848+kristinapunko@users.noreply.github.com>
2025-05-28 14:25:24 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# абсолютний шлях для збирання статичних файлів
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
