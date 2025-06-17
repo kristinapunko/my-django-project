bb2e6469b5b69df56421a2ca86ebdc9641090d4f
kristinapunko <kristina.punko2006@gmail.com>
2025-06-17 17:56:29 +0300

    Fix media

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tours/', include('tours.urls')), 
     path("api/", include("accounts.urls")),
     path("cabinet/", include('user_cabinet.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Для розробки (необов’язково, якщо DEBUG = False)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
